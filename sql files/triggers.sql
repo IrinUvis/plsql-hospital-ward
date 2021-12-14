9d298fc80c607663dce4ab4ca394108dae1ee711
lecheliza <lechoosz@gmail.com>
2021-12-14 13:18:09 +0100

    placed tests in one place and updated hospital package

--- sql files/triggers.sql
+++ sql files/triggers.sql
@@ -14,15 +14,6 @@ BEGIN
     END IF;
 END;
 
---test
-    --the 'F' should be changed to 'M'
-    insert into Patients patients (
-    patient_id, first_name, last_name, gender, date_of_birth, phone_number)
-    VALUES ('12301378910', 'y', 'z', 'F', '07-JUL-1990', 123456789);
-
-select * from patients;
-rollback;
-
 --trigger 2: if the discharge date is earlier than the registration, react 
 CREATE OR REPLACE TRIGGER visit_dates_trg BEFORE INSERT OR UPDATE ON visits
 FOR EACH ROW 
@@ -33,26 +24,6 @@ BEGIN
         :NEW.discharge_date := :NEW.registration_date;
     END IF;
 END;
-
---test
-INSERT INTO visits (
-    visit_id, doctor_id, patient_id, registration_date, discharge_date)
-    VALUES (
-    820384, 100, 99112799315, '09-NOV-21', '08-SEP-21');
-    
-    select * from Visits where visit_id = 820384;
-    
-    update visits set discharge_date = '08-OCT-07' where visit_id = 820384;
-
-    rollback;
-    
-    --check if the date is really inserted when the value is not provided
-    INSERT INTO visits (
-    visit_id, doctor_id, patient_id)
-    VALUES (
-    820386, 100, 99112799315);
-    select * from visits where visit_id = 820386;
-    rollback;
     
 --trigger 3: check if the salary is between the range for the given specialization
 CREATE OR REPLACE TRIGGER salary_check BEFORE INSERT OR UPDATE ON Doctors
@@ -68,24 +39,6 @@ BEGIN
         :NEW.salary := min_sal;
     END IF;
 END;
-
---test 1: number too small
-select * from specializations where specialization_id = 'NRLG';
-INSERT INTO doctors (
-    doctor_id, specialization_id, first_name, last_name, gender, date_of_birth, phone_number, hire_date, salary)
-    VALUES (
-    10001, 'NRLG', 'Mariam', 'Navaro', 'F', '15-FEB-1975', 123654789, '12-MAY-2017', 6000);
-select * from doctors where doctor_id = 10001;
-rollback;
-
---test 2: number too high
-select * from specializations where specialization_id = 'NRLG';
-INSERT INTO doctors (
-    doctor_id, specialization_id, first_name, last_name, gender, date_of_birth, phone_number, hire_date, salary)
-    VALUES (
-    10001, 'NRLG', 'Mariam', 'Navaro', 'F', '15-FEB-1975', 123654789, '12-MAY-2017', 40000);
-select * from doctors where doctor_id = 10001;
-rollback;
 
 --trigger 4: check and put proper birth date based on the pesel
 CREATE OR REPLACE TRIGGER check_birth_date_trg BEFORE INSERT OR UPDATE ON Patients
@@ -103,11 +56,3 @@ BEGIN
         :NEW.date_of_birth := l_date;
     END IF;
 END;
-
-
---try to insert person who was (will be?) born in 2083 and is a male 
- insert into Patients patients (
-    patient_id, first_name, last_name, gender, date_of_birth, phone_number)
-    VALUES ('83223178910', 'x', 'y', 'F', '07-JUL-1990', 123456789);
-select * from Patients where first_name = 'x';
-rollback;
